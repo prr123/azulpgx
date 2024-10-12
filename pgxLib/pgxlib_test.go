7a91316bb5dd2ac401ae22a4c7b2d72fa5c82283
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-10-12 12:56:57 +0200

    fix problem with null results via coalesce

--- pgxLib/pgxlib_test.go
+++ pgxLib/pgxlib_test.go
@@ -3,6 +3,7 @@
 package pgxlib
 
 import (
+	"fmt"
 	"testing"
 	"context"
 	"github.com/jackc/pgx/v5"
@@ -21,7 +22,50 @@ func TestInit(t *testing.T) {
 	if err != nil {t.Fatalf("error -- InitPgLib: %v\n", err)}
 
 	if pglib.con == nil {t.Fatalf("error -- Unable to init pflib\n")}
-
-
 }
 
+func TestGetCols(t *testing.T) {
+
+    ctx := context.Background()
+    dburl :="postgresql://dbuser:dbtest@/testdb"
+    dbcon, err := pgx.Connect(ctx, dburl)
+    if err != nil {t.Fatalf("error -- Unable to create connection: %v\n", err)}
+    defer dbcon.Close(ctx)
+
+    pglib, err := InitPgLib(dbcon)
+    if err != nil {t.Fatalf("error -- InitPgLib: %v\n", err)}
+//	fmt.Println("init")
+	pglib.ctx = ctx
+
+	pgcol, err := pglib.GetColInfo("person")
+    if err != nil {t.Fatalf("error -- GetColInfo: %v\n", err)}
+
+	fmt.Printf("columns: %d\n", len(pgcol))
+	for i:=0; i<len(pgcol); i++ {
+		fmt.Printf("col[%d]: %s %d %d\n",i, pgcol[i].ColName, pgcol[i].ColWidth, pgcol[i].Prec)
+
+	}
+}
+
+func TestGetCols2(t *testing.T) {
+
+    ctx := context.Background()
+    dburl :="postgresql://dbuser:dbtest@/testdb"
+    dbcon, err := pgx.Connect(ctx, dburl)
+    if err != nil {t.Fatalf("error -- Unable to create connection: %v\n", err)}
+    defer dbcon.Close(ctx)
+
+    pglib, err := InitPgLib(dbcon)
+    if err != nil {t.Fatalf("error -- InitPgLib: %v\n", err)}
+//	fmt.Println("init")
+	pglib.ctx = ctx
+
+	pgcol, err := pglib.GetColInfoV2("person")
+    if err != nil {t.Fatalf("error -- GetColInfo: %v\n", err)}
+
+	fmt.Printf("columns: %d\n", len(pgcol))
+	for i:=0; i<len(pgcol); i++ {
+		fmt.Printf("col[%d]: %-15s %-20s %d %d\n",i, pgcol[i].ColName, pgcol[i].ColType, pgcol[i].ColWidth, pgcol[i].Prec)
+
+	}
+}
